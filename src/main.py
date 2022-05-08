0d44f3393e40c36261eabe773af650a49da35a36
allan <alanhe159@gmail.com>
2022-05-08 00:48:12 -0400

    change color

--- src/main.py
+++ src/main.py
@@ -48,7 +48,7 @@ async def hello(ctx):
 
 @client.command()
 async def help(ctx, arg1 = ""):
-  embed=discord.Embed(title="This is Study Oppa's default help menu.", description="Choose one of the options below to learn more", color=0x6bb3ff)
+  embed=discord.Embed(title="This is Study Oppa's default help menu.", description="Choose one of the options below to learn more", color=0x8EA8FB)
   embed.add_field(name="flashcards", value="flashcards but in Discord!", inline=False)
   embed.add_field(name="pomodoro", value="advanced studying technique", inline=False)
   embed.add_field(name="todolist", value="keeps track of what's left to do", inline=False)
